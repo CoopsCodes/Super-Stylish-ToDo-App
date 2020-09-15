e2434134f358e6b50e55e7bb5f42cff067c0c928
Cooper Viktor <im.cooperviktor@gmail.com>
2020-09-15 18:08:05 +1000

    Updates

---
+++ precache-manifest.a490996545483f972feab7a9ff6c9605.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a796bc447c3f13c95e76c272a0bcb67",
+    "url": "/Super-Stylish-ToDo-App/index.html"
+  },
+  {
+    "revision": "b0728e0ad7fd48c07e7f",
+    "url": "/Super-Stylish-ToDo-App/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "f5d59bcd1936b5d1127c",
+    "url": "/Super-Stylish-ToDo-App/static/css/main.143ecbe2.chunk.css"
+  },
+  {
+    "revision": "b0728e0ad7fd48c07e7f",
+    "url": "/Super-Stylish-ToDo-App/static/js/2.0447e89c.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/Super-Stylish-ToDo-App/static/js/2.0447e89c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5d59bcd1936b5d1127c",
+    "url": "/Super-Stylish-ToDo-App/static/js/main.871ffb26.chunk.js"
+  },
+  {
+    "revision": "bc4a2f51e8b0d565fcc6",
+    "url": "/Super-Stylish-ToDo-App/static/js/runtime-main.a27faf48.js"
+  }
+]);
